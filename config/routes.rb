da0e9a6dd051b01fd981cb2083366d7a17914cbd
benja287 <rodriguezmantilla123@gmail.com>
2023-11-02 14:20:52 -0300

    se arreglo la vista el  login abajo

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,18 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'main#home'
+  #root 'main#home'
+  root "main#home"
 
   resources :cuidadores, only: [:show,:index, :new, :create]
   resources :paseadores, only: [:index,:show,:new, :create, :edit, :update]
 
-
+  get 'main/home', to: 'main#home'
 
 
   # Defines the root path route ("/")
-    get '/home', to: 'application#home'
-    get '/homeAdopcion', to: 'application#homeAdopcion'
-    get '/homeCampana', to: 'application#homeCampana'
+  get '/home', to: 'application#home'
+  get '/homeAdopcion', to: 'application#homeAdopcion'
+  get '/homeCampana', to: 'application#homeCampana'
 
-  # root "articles#index"
-  #root "application#home"
 
 end
