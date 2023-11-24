2dfbddffb5ab06070c7e7799262cd754e9c217dc
benja287 <rodriguezmantilla123@gmail.com>
2023-11-24 14:07:42 -0300

    preparado para la demo

--- app/models/paseador.rb
+++ app/models/paseador.rb
@@ -1,3 +1,3 @@
 class Paseador < ApplicationRecord
-  validates_uniqueness_of :email, message: "Ya existe un cuidador con este email."
+  #validates_uniqueness_of :email, message: "Ya existe un cuidador con este email."
 end
