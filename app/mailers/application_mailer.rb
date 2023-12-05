ee342a9bffff4cf6b4cb6124a639ac09e10ec16c
Byron_Pico <alex.brandon67@ymail.com>
2023-12-05 13:04:42 -0300

    acutalizacion 5 dic

--- app/mailers/application_mailer.rb
+++ app/mailers/application_mailer.rb
@@ -1,4 +1,4 @@
 class ApplicationMailer < ActionMailer::Base
-  default from: "from@example.com"
+  default from: "no_reply@ohmygod.com"
   layout "mailer"
 end
--- app/models/encontrado.rb
+++ app/models/encontrado.rb
@@ -1,22 +1,31 @@
 class Encontrado < ApplicationRecord
     has_one_attached :photo
+    
     validate :validacion_perros_no_repetidos
     validates :nombre, length: { maximum: 25, message: "La longitud del nombre debe ser mas de 25 caracteres" }
-    validates :ubicacion, length: { maximum: 70, message: "La longitud de la ubicacion debe ser mas de 70 caracteres" }
+  
     validates :raza, length: { maximum: 30, message: "La longitud de la raza debe ser mas de 25 caracteres" }
     validates :nombre, presence: true, presence: { message: "no puede estar en blanco" }
     validates :tamano, presence: true, presence: { message: "no puede estar en blanco" }
-
+    
     belongs_to :user
 
     validates :raza, presence: true, presence: { message: "no puede estar en blanco" }
     validates :ubicacion, presence: true, presence: { message: "no puede estar en blanco" }
-    private
+    
 
+    #codigo nuevo
+    
+    validates :email, presence: true, format: { with: URI::MailTo::EMAIL_REGEXP }
+    validates :codigo, presence: true
+    private
     def validacion_perros_no_repetidos
-    # Se asume que `autor_id` es un atributo en la instancia de `Perdido`
-      if User.find(user_id).encontrados.exists?(nombre: nombre)
+    # no repetidos por nombre y email
+      if User.find(user_id).encontrados.exists?(nombre: nombre, email: email)
         errors.add(:base, "Ya hiciste una publicacion de este perrito")
       end
     end
+    
+    # codigo nuevo
+
  end
